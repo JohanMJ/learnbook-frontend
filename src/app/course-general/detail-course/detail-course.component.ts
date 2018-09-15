4d76899cd1b770a64ef9ffc8ac6eca338171f904
Matheus <matheusjohanmulder@gmail.com>
2018-09-15 16:42:50 -0300

    Improvements
    Alignments and implements search pipe on listAll page.

--- src/app/course-general/detail-course/detail-course.component.ts
+++ src/app/course-general/detail-course/detail-course.component.ts
@@ -1,20 +1,10 @@
 import { Course } from '../../models/course';
 import { DataService } from '../../data.service';
 import { Component, OnInit } from '@angular/core';
-import {
-  Directive, forwardRef,
-  Attribute, OnChanges, SimpleChanges, Input
-} from '@angular/core';
-import {
-  NG_VALIDATORS, Validator,
-  Validators, AbstractControl, ValidatorFn
-} from '@angular/forms';
+import { Directive, forwardRef, Attribute, OnChanges, SimpleChanges, Input } from '@angular/core';
+import { NG_VALIDATORS, Validator, Validators, AbstractControl, ValidatorFn } from '@angular/forms';
 import { ActivatedRoute, Params } from '@angular/router';
 import { PdfViewerModule } from 'ng2-pdf-viewer';
-
-
-
-
 
 @Component({
   selector: 'app-detail-course',
@@ -28,16 +18,16 @@ export class DetailCourseComponent implements OnInit {
     page: number = 1;
     totalPages: number;
     isLoaded: boolean = false;
-  
+
     afterLoadComplete(pdfData: any) {
       this.totalPages = pdfData.numPages;
       this.isLoaded = true;
     }
-  
+
     nextPage() {
       this.page++;
     }
-  
+
     prevPage() {
       this.page--;
     }
@@ -45,12 +35,16 @@ export class DetailCourseComponent implements OnInit {
     constructor(private dataService: DataService, private route: ActivatedRoute) {
       this.id = Number (this.route.snapshot.paramMap.get('iCodCou'));
     }
-  
+
     getCourseById() {
       return this.dataService.getCourseById(this.id).then(currentCourse => this.currentCourse = currentCourse);
-     
-   }
-  
+
+    }
+
+    accomplishTask() {
+      // set task accomplished
+    }
+
     ngOnInit(): void {
        console.log(this.getCourseById());
   }
